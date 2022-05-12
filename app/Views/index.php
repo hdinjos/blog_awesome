3ec9eaaeb91148343e7e80fb5196f6790856e4d0
hdinjos <hdinjos@gmail.com>
2022-05-12 13:51:53 +0700

    change router

---
+++ app/Views/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Awesome blog</title>
+</head>
+
+<body>
+  <h2>Wellcome my blog ##:)##</h2>
+</body>
+
+</html>
