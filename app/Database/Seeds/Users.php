a12ad509a155f46bbb95250e85562a45ff144397
hdinjos <hdinjos@gmail.com>
2022-05-13 07:24:19 +0700

    change seeder users field password

--- app/Database/Seeds/Users.php
+++ app/Database/Seeds/Users.php
@@ -14,14 +14,14 @@ class Users extends Seeder
                 'name' => 'Admin blog',
                 'bio' => 'Admin blog keren :)',
                 'email' => 'admin@awesomeblog.com',
-                'password' => '123',
+                'password' => '$2y$10$HiY6ctTsFHGlaIaSaWG5EO4VPHzEJvSYOi.TNegHqU3P/S/1Q6d9C', //value = 123
                 'role_id' => 1
             ],
             [
                 'name' => 'Ilham Saqoc',
                 'bio' => 'Penulis pertama di blog ini',
                 'email' => 'ilhams@awesomeblog.com',
-                'password' => '123',
+                'password' => '$2y$10$siuZrRhWfY87m0v4nJJyuuRFVIVLuvUFjWJhLUDuFxQ8AgEMFnMUm', //value = 123
                 'role_id' => 2
             ]
         ];
