4c9c2f4aef09a94043474ab8e9309430c6263100
hdinjos <hdinjos@gmail.com>
2022-05-19 21:15:56 +0700

    make layouting for frontpage

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Awesome blog</title>
-</head>
-
-<body>
-  <h2>Wellcome my blog ##:)##</h2>
-</body>
-
-</html>
+<?= $this->extend('layout/layout_front') ?>
+<?= $this->section('content') ?>
+<h1>Wellcome to my blog</h1>
+<?= $this->endSection() ?>
---
+++ app/Views/layout/header_front.php
@@ -0,0 +1,5 @@
+<div>
+  <div><a href="/">Home</a></div>
+  <div><a href="/categories">Category</a></div>
+  <div><a href="/login">Login</a></div>
+</div>
