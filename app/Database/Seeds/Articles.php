f057a15f716fce5f524024041afea809cb966865
hdinjos <hdinjos@gmail.com>
2022-05-30 00:55:02 +0700

    change seeder in articles

--- app/Database/Seeds/Articles.php
+++ app/Database/Seeds/Articles.php
@@ -10,22 +10,37 @@ class Articles extends Seeder
     {
         //
         $datas = [
-            [
-                'title' => 'Twitter Diakuisisi Elon Musk',
+            // [
+            //     'title' => 'Twitter Diakuisisi Elon Musk',
+            //     'content' => 'Jakarta, sebagian besar saham twitter baru-baru ini dibeli oleh orang terkaya sedunia yaitu Elon Musk',
+            //     'author_id' => 1,
+            //     'slug' => 'twitter-diakuisisi-elon-musk',
+            //     'status' => 'publish',
+            //     'category_id' => 1
+            // ],
+            // [
+            //     'title' => 'Film Wuxia Terbaru',
+            //     'content' => 'Who Rules The Word akan dirilis bulan Mei ini oleh Tencen Picture, menampilkan Yang-Yang dan Zhao Luci',
+            //     'author_id' => 2,
+            //     'slug' => 'film-wuxia-terbaru', 
+            //     'category_id' => 2
+            // ]
+        ];
+
+        for ($i = 0; $i <= 15; $i++) {
+            $arr = [
+                'title' => 'Twitter Diakuisisi Elon Musk' . $i,
                 'content' => 'Jakarta, sebagian besar saham twitter baru-baru ini dibeli oleh orang terkaya sedunia yaitu Elon Musk',
                 'author_id' => 1,
                 'slug' => 'twitter-diakuisisi-elon-musk',
                 'status' => 'publish',
                 'category_id' => 1
-            ],
-            [
-                'title' => 'Film Wuxia Terbaru',
-                'content' => 'Who Rules The Word akan dirilis bulan Mei ini oleh Tencen Picture, menampilkan Yang-Yang dan Zhao Luci',
-                'author_id' => 2,
-                'slug' => 'film-wuxia-terbaru',
-                'category_id' => 2
-            ]
-        ];
+            ];
+
+            array_push($datas, $arr);
+            // echo $num[$i];
+        }
+
         foreach ($datas as $data) {
             $this->db->table('articles')->insert($data);
         };
